9ba9d545bc97ab40b1ee1d4c3c5a6e8c2d4198aa
Supakorn Kaewjaeng <nathsupakorn@gmail.com>
2021-11-27 01:25:13 +0700

    signup strat page

--- frontend/src/views/signup/signup.js
+++ frontend/src/views/signup/signup.js
@@ -3,12 +3,47 @@ import '../../App.css'
 
 const Signup = () => {
   return (
-    <div>
-      <div class="container">
-        <h1 class="chuose" style={{ color: COLORS.RED }}>
-          CHUOSE
-        </h1>
-        <button class="button">START</button>
+    <div class="center">
+      <div class="main">
+        <img id="bg1" src={'images/BG_01.svg'} />
+        <img id="cloud" src={'images/Clouds.svg'} />
+        <img id="bg2" src={'images/BG_02.svg'} />
+        <div class="container">
+          <h1 class="allChuose">
+            <div class="chuose" style={{ color: COLORS.RED }}>
+              C
+            </div>
+            <div class="chuose" style={{ color: COLORS.BLUE }}>
+              H
+            </div>
+            <div class="chuose" style={{ color: COLORS.ORANGE }}>
+              U
+            </div>
+            <div class="chuose" style={{ color: COLORS.PURPLE }}>
+              O
+            </div>
+            <div class="chuose" style={{ color: COLORS.PINK }}>
+              S
+            </div>
+            <div class="chuose" style={{ color: COLORS.GREEN }}>
+              E
+            </div>
+          </h1>
+          <div class="center" id="imgChar02">
+            <img src={'images/Charactor.svg'} />
+          </div>
+          <div class="center">
+            <p class="paragraph">
+              Hi there!
+              <br />
+              Everyone are deserve to be happy <br />
+              Everyone are deserve to be happy
+            </p>
+          </div>
+          <div>
+            <button class="button">START</button>
+          </div>
+        </div>
       </div>
     </div>
   )
