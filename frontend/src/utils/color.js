31a42a2d3e749f1876846a0ea89b35e72dd3d363
Supakorn Kaewjaeng <nathsupakorn@gmail.com>
2021-11-26 22:18:53 +0700

    demo start page

--- frontend/src/utils/color.js
+++ frontend/src/utils/color.js
@@ -1,6 +1,24 @@
-const GREEN = 'rgb(193, 205, 118)'
+const GREEN = '#BDCE71'
+const PINK = '#FAC0AE'
+const YELLOW = '#FDE642'
+const PURPLE = '#C185BA'
+const ORANGE = '#FFC812'
+const BLUE = '#AFC9E9'
+const RED = '#F0524A'
+const VALID = '#3C5676'
+const INVALID = '#8A9CB2'
+const TEXT = '#425F83'
 
 const COLORS = {
   GREEN,
+  PINK,
+  YELLOW,
+  PURPLE,
+  ORANGE,
+  BLUE,
+  RED,
+  VALID,
+  INVALID,
+  TEXT,
 }
 export default COLORS
--- frontend/src/views/signup/signup.js
+++ frontend/src/views/signup/signup.js
@@ -1,7 +1,17 @@
 import COLORS from '../../utils/color'
+import '../../App.css'
 
 const Signup = () => {
-  return <div></div>
+  return (
+    <div>
+      <div class="container">
+        <h1 class="chuose" style={{ color: COLORS.RED }}>
+          CHUOSE
+        </h1>
+        <button class="button">START</button>
+      </div>
+    </div>
+  )
 }
 
 export default Signup
