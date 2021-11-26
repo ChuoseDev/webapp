7d8459504698be938f66100919f0ecd9836a107b
SuppanatP <suppanat-pun@hotmail.com>
2021-11-27 02:16:32 +0700

    feat: info page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,15 @@
 import logo from './logo.svg'
 import './App.css'
 import { BrowserRouter, Routes as Switch, Route } from 'react-router-dom'
-import Signup from './views/signup/signup'
+//import Signup from './views/signup/signup'
+import Info from './views/info/info'
 
 const App = () => {
   return (
     <div className="App">
       <BrowserRouter>
         <Switch>
-          <Route exact path="/" element={<Signup />}></Route>
+          <Route exact path="/" element={<Info />}></Route>
         </Switch>
       </BrowserRouter>
     </div>
