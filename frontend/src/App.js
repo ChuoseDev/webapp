8567a254aa3d644fc618a00be5f2c688c62a0949
KonKinPak <tiewclub9@gmail.com>
2022-01-13 20:51:43 +0700

    add_home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg'
 import './App.css'
 import { BrowserRouter, Routes as Switch, Route } from 'react-router-dom'
 //import Signup from './views/signup/signup'
+import Home from './views/signup/home'
 import Info from './views/info/info'
 import Signup from './views/signup/signup'
 import Loading from './views/signup/loading'
@@ -14,6 +15,7 @@ const App = () => {
           <Route exact path="/" element={<Info />}></Route>
           <Route exact path="/signup" element={<Signup />}></Route>
           <Route exact path="/loading" element={<Loading />}></Route>
+          <Route exact path="/home" element={<Home />}></Route>
         </Switch>
       </BrowserRouter>
     </div>
