862d845994c0a7fe08807a0a863589982dc87223
pemtut <pemzar007@hotmail.com>
2022-01-16 01:54:49 +0700

    edit questionnaire page to responsive

--- frontend/src/views/questionnaire/feeling.js
+++ frontend/src/views/questionnaire/feeling.js
@@ -1,19 +1,28 @@
-import COLORS from '../../utils/color'
 import './feeling.css'
 
 
+
 const Feeling = () => {
+
+    const onClickLetGo= async () => {
+        try {
+        console.log(document.getElementById('feeling-textarea').value)
+        } catch (err) {
+        console.log(err)
+        }
+    }
+    
     return (
         <div class='container'>
-            <img src={'images/BG_04.svg'}/>
+            <img class="bg4" src={'images/BG_04.svg'}/>
             <div class='charactor'>
                 <img class='charactor-size' src={'images/Charactor.svg'}/>
             </div>
             <div class='question'>How do you feel today?<br/>you can talk to me ...</div>
-            <textarea class="textbox" placeholder='Type here ...'></textarea>
+            <textarea id='feeling-textarea' class="textbox" placeholder='Type here ...'></textarea>
             <div class='center'>
                 <div class='bottom'>
-                    <button class='button'>Let's go!</button>
+                    <button class='button' onClick={() => onClickLetGo()} >Let's go!</button>
                 </div>
             </div>
             <img class="cloud" src={'images/Clouds.svg'}/>
