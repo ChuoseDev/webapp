8867fb90322dace2b4165bcd369a5ac317df76a5
pemtut <pemzar007@hotmail.com>
2022-01-23 02:18:11 +0700

    Format with prettier in questionnaire page

--- frontend/src/views/questionnaire/feeling.js
+++ frontend/src/views/questionnaire/feeling.js
@@ -1,33 +1,40 @@
 import './feeling.css'
 
-
-
 const Feeling = () => {
-
-    const onClickLetGo= async () => {
-        try {
-        console.log(document.getElementById('feeling-textarea').value)
-        } catch (err) {
-        console.log(err)
-        }
+  const onClickLetGo = async () => {
+    try {
+      console.log(document.getElementById('feeling-textarea').value)
+    } catch (err) {
+      console.log(err)
     }
-    
-    return (
-        <div class='container'>
-            <img class="bg4" src={'images/BG_04.svg'}/>
-            <div class='charactor'>
-                <img class='charactor-size' src={'images/Charactor.svg'}/>
-            </div>
-            <div class='question'>How do you feel today?<br/>you can talk to me ...</div>
-            <textarea id='feeling-textarea' class="textbox" placeholder='Type here ...'></textarea>
-            <div class='center'>
-                <div class='bottom'>
-                    <button class='button' onClick={() => onClickLetGo()} >Let's go!</button>
-                </div>
-            </div>
-            <img class="cloud" src={'images/Clouds.svg'}/>
+  }
+
+  return (
+    <div class="container">
+      <img class="bg4" src={'images/BG_04.svg'} />
+      <div class="charactor">
+        <img class="charactor-size" src={'images/Charactor.svg'} />
+      </div>
+      <div class="question">
+        How do you feel today?
+        <br />
+        you can talk to me ...
+      </div>
+      <textarea
+        id="feeling-textarea"
+        class="textbox"
+        placeholder="Type here ..."
+      ></textarea>
+      <div class="center">
+        <div class="bottom">
+          <button class="button" onClick={() => onClickLetGo()}>
+            Let's go!
+          </button>
         </div>
-    )
+      </div>
+      <img class="cloud" src={'images/Clouds.svg'} />
+    </div>
+  )
 }
 
 export default Feeling
