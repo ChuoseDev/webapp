4ff491b559d8ca16477622cb37eedfb1557663f9
SuppanatP <suppanat-pun@hotmail.com>
2022-03-27 16:40:38 +0700

    Scene03 Starter

---
+++ frontend/src/views/scene03/scene03_01.js
@@ -0,0 +1,12 @@
+import './purple.css'
+
+export const Scene03_01 = () => {
+  return (
+    <div class="container">
+      <img class="bg_purple" src={'images/BG_Purple.svg'} />
+      <img class="star" src={'images/star.svg'} />
+      <img class="clouds" src={'images/4Clouds.svg'} />
+      <div class="text">กุ๊งกิ๊ง กุ๊งกิ๊ง</div>
+    </div>
+  )
+}
