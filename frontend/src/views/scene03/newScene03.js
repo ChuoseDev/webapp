66fc4c3128d4c50e95b22ad1b6b266d2115923a0
pemtut <pemzar007@hotmail.com>
2022-04-03 16:34:28 +0700

    edit scene03_01-03_06

--- frontend/src/views/scene03/newScene03.js
+++ frontend/src/views/scene03/newScene03.js
@@ -24,35 +24,15 @@ const Sscene03 = () => {
     setTimeout(() => {
       setScene(6)
     }, 2500)
-    setTimeout(() => {
-      setScene(7)
-    }, 3000)
-    setTimeout(() => {
-      setScene(8)
-    }, 3500)
-    setTimeout(() => {
-      setScene(9)
-    }, 4000)
-    setTimeout(() => {
-      setScene(10)
-    }, 4500)
-    setTimeout(() => {
-      setScene(11)
-    }, 5000)
-    setTimeout(() => {
-      navigate('/scene041')
-    }, 7000)
   }, [])
 
   const containerStyle = (scene) => {
     return {
-      width: window.innerWidth,
-      height: window.innerHeight,
       position: 'relative',
       left: 0,
       top: 0,
-      margin: 0,
       textAlign: 'center',
+      margin: 0,
     }
   }
 
@@ -106,7 +86,197 @@ const Sscene03 = () => {
         )
       }
       case 2: {
-        return(
+        return (
+          <div>
+            <img
+              class="cloud01"
+              src={'images/WhiteCloud01.svg'}
+              alt="background"
+              style={{ top: '16.82%', left: '31.79%' }}
+            />
+            <img
+              class="cloud01"
+              src={'images/WhiteCloud01.svg'}
+              alt="background"
+              style={{ top: '35.78%', left: '-4.87%' }}
+            />
+            <img
+              class="cloud02"
+              src={'images/WhiteCloud02.svg'}
+              alt="background"
+              style={{ top: '61.72%', left: '30.77%' }}
+            />
+            <img
+              class="cloud03"
+              src={'images/WhiteCloud03.svg'}
+              alt="background"
+              style={{ top: '87.32%', left: '11.54%' }}
+            />
+            <div class="text_center">กุ๊งกิ๊ง กุ๊งกิ๊ง</div>
+          </div>
+        )
+      }
+
+      case 3: {
+        return (
+          <div>
+            <img
+              class="cloud01"
+              src={'images/WhiteCloud01.svg'}
+              alt="background"
+              style={{ top: '12.44%', left: '31.54%' }}
+            />
+            <img
+              class="cloud01"
+              src={'images/WhiteCloud01.svg'}
+              alt="background"
+              style={{ top: '29.27%', left: '-8.89%' }}
+            />
+            <img
+              class="cloud02"
+              src={'images/WhiteCloud02.svg'}
+              alt="background"
+              style={{ top: '55.33%', left: '45.38%' }}
+            />
+            <img
+              class="cloud03"
+              src={'images/WhiteCloud03.svg'}
+              alt="background"
+              style={{ top: '80.69%', left: '2.56%' }}
+            />
+            <div class="text_center">กุ๊งกิ๊ง กุ๊งกิ๊ง</div>
+          </div>
+        )
+      }
+
+      case 4: {
+        return (
+          <div>
+            <img
+              class="cloud02"
+              src={'images/WhiteCloud02.svg'}
+              alt="background"
+              style={{ top: '7.7%', left: '52.82%' }}
+            />
+            <img
+              class="cloud01"
+              src={'images/WhiteCloud01.svg'}
+              alt="background"
+              style={{ top: '30.21%', left: '-7.44%' }}
+            />
+            <img
+              class="cloud01"
+              src={'images/WhiteCloud01.svg'}
+              alt="background"
+              style={{ top: '62.09%', left: '43.08%' }}
+            />
+
+            <img
+              class="cloud03"
+              src={'images/WhiteCloud03.svg'}
+              alt="background"
+              style={{ top: '84.12%', left: '-2.05%' }}
+            />
+            <img
+              class="charactor"
+              src={'images/Charactor.svg'}
+              alt="charactor"
+              style={{
+                top: '-18.5%',
+                left: '-3.85%',
+                transform: 'rotate(-22deg)',
+                width: '185px',
+              }}
+            />
+            <div class="text_center">เสียงอะไรน่ะ</div>
+          </div>
+        )
+      }
+
+      case 5: {
+        return (
+          <div>
+            <img
+              class="cloud01"
+              src={'images/WhiteCloud01.svg'}
+              alt="background"
+              style={{ top: '8.41%', left: '71.28%' }}
+            />
+            <img
+              class="cloud02"
+              src={'images/WhiteCloud02.svg'}
+              alt="background"
+              style={{ top: '30.21%', left: '-7.44%' }}
+            />
+            <img
+              class="cloud01"
+              src={'images/WhiteCloud01.svg'}
+              alt="background"
+              style={{ top: '62.09%', left: '43.08%' }}
+            />
+
+            <img
+              class="cloud03"
+              src={'images/WhiteCloud03.svg'}
+              alt="background"
+              style={{ top: '84.12%', left: '-2.05%' }}
+            />
+            <img
+              class="charactor"
+              src={'images/Charactor.svg'}
+              alt="charactor"
+              style={{
+                top: '-20%',
+                left: '2.93%',
+                transform: 'rotate(-22deg)',
+                width: '284.44px',
+              }}
+            />
+            <div class="text_center">เสียงอะไรน่ะ</div>
+          </div>
+        )
+      }
+
+      case 6: {
+        return (
+          <div>
+            <img
+              class="cloud01"
+              src={'images/WhiteCloud01.svg'}
+              alt="background"
+              style={{ top: '12.44%', left: '66.92%' }}
+            />
+            <img
+              class="cloud02"
+              src={'images/WhiteCloud02.svg'}
+              alt="background"
+              style={{ top: '33.89%', left: '-22.31%' }}
+            />
+            <img
+              class="cloud01"
+              src={'images/WhiteCloud01.svg'}
+              alt="background"
+              style={{ top: '58.18%', left: '52.05%' }}
+            />
+
+            <img
+              class="cloud03"
+              src={'images/WhiteCloud03.svg'}
+              alt="background"
+              style={{ top: '87.09%', left: '-17.69%' }}
+            />
+            <img
+              class="charactor"
+              src={'images/Charactor.svg'}
+              alt="charactor"
+              style={{
+                top: '-15%',
+                left: '7%',
+                transform: 'rotate(-22deg)',
+                width: '284.44px',
+              }}
+            />
+          </div>
         )
       }
 
@@ -114,19 +284,6 @@ const Sscene03 = () => {
         return <div></div>
       }
     }
-    return <div></div>
-  }
-
-  const whiteCloud02 = (scene) => {
-    return
-  }
-
-  const whiteCloud03 = (scene) => {
-    return
-  }
-
-  const whiteCloud04 = (scene) => {
-    return
   }
 
   return (
