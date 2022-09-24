64d900bbb9318b424960b53013b4ea0b60cab96d
Suppanat Poo-im <suppanat.pm@gmail.com>
2022-09-24 15:23:40 +0700

    Add analyze scene

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -15,6 +15,7 @@ export const SCENE11_SHIFT_TIME = 1000
 export const SCENE12_SHIFT_TIME = 1500
 export const SCENE13_SHIFT_TIME = 1000
 export const SCENE15_SHIFT_TIME = 1000
+export const ANALYSE_SHIFT_TIME = 1000
 
 // components scene shifter interval time
 export const STAR_SHIFT_TIME = 500
